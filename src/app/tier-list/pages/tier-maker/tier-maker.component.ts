335fd00e3be37c91dec81b967bb947429f6b434b
andresma <andresma2490@gmail.com>
2023-11-09 21:49:04 -0500

    feat: add drag & drop feature in tier-maker page

--- src/app/tier-list/pages/tier-maker/tier-maker.component.ts
+++ src/app/tier-list/pages/tier-maker/tier-maker.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 import { TierListMockService } from '../../services/tier-list-mock.service';
-import { TierItem, TierTable } from '../../models/tier-list.model';
+import { TierItem, TierRow } from '../../models/tier-list.model';
 
 @Component({
   selector: 'app-tier-maker',
@@ -11,7 +16,7 @@ import { TierItem, TierTable } from '../../models/tier-list.model';
 export class TierMakerComponent implements OnInit {
   id = '';
   title = '';
-  table: TierTable = [];
+  table: TierRow[] = [];
   items: TierItem[] = [];
 
   constructor(
@@ -26,5 +31,28 @@ export class TierMakerComponent implements OnInit {
       this.table = data.table;
       this.items = data.items;
     });
+  }
+
+  moveItem(event: CdkDragDrop<TierItem[]>) {
+    const { previousContainer, container, previousIndex, currentIndex } = event;
+    const sameContainer = previousContainer === container;
+    if (sameContainer && previousIndex === currentIndex) return;
+    if (sameContainer) {
+      moveItemInArray(container.data, previousIndex, currentIndex);
+    } else {
+      transferArrayItem(
+        previousContainer.data,
+        container.data,
+        previousIndex,
+        currentIndex,
+      );
+    }
+  }
+
+  moveRow(event: CdkDragDrop<TierRow[]>) {
+    const { container, previousIndex, currentIndex } = event;
+    if (previousIndex === currentIndex) return;
+
+    moveItemInArray(container.data, previousIndex, currentIndex);
   }
 }
--- src/app/tier-list/pages/tier-maker/tier-maker.module.ts
+++ src/app/tier-list/pages/tier-maker/tier-maker.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 import { TierMakerRoutingModule } from './tier-maker-routing.module';
 import { TierMakerComponent } from './tier-maker.component';
 import { TierListComponentsModule } from '../../components/tier-list-components.module';
-
 @NgModule({
   declarations: [TierMakerComponent],
-  imports: [CommonModule, TierMakerRoutingModule, TierListComponentsModule],
+  imports: [
+    CommonModule,
+    DragDropModule,
+    TierMakerRoutingModule,
+    TierListComponentsModule,
+  ],
 })
 export class TierMakerModule {}
